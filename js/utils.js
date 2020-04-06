12e9728f459e68489930814c21f05770167c352a
q1021185180 <Z15231234547@163.com>
2020-04-06 23:41:20 +0800

    Site updated: 2020-04-06 23:41:19

--- js/utils.js
+++ js/utils.js
@@ -53,7 +53,6 @@ NexT.utils = {
       link.rel = 'noopener external nofollow noreferrer';
       link.target = '_blank';
       link.className = element.className;
-      link.title = element.title;
       link.innerHTML = element.innerHTML;
       element.parentNode.replaceChild(link, element);
     });
@@ -67,7 +66,7 @@ NexT.utils = {
       const box = document.createElement('div');
       element.wrap(box);
       box.classList.add('highlight-container');
-      box.insertAdjacentHTML('beforeend', '<div class="copy-btn"><i class="fa fa-clipboard fa-fw"></i></div>');
+      box.insertAdjacentHTML('beforeend', '<div class="copy-btn"><i class="fa fa-clipboard"></i></div>');
       var button = element.parentNode.querySelector('.copy-btn');
       button.addEventListener('click', event => {
         var target = event.currentTarget;
@@ -86,7 +85,7 @@ NexT.utils = {
         ta.readOnly = false;
         var result = document.execCommand('copy');
         if (CONFIG.copycode.show_result) {
-          target.querySelector('i').className = result ? 'fa fa-check fa-fw' : 'fa fa-times fa-fw';
+          target.querySelector('i').className = result ? 'fa fa-check' : 'fa fa-times';
         }
         ta.blur(); // For iOS
         target.blur();
@@ -98,7 +97,7 @@ NexT.utils = {
       });
       button.addEventListener('mouseleave', event => {
         setTimeout(() => {
-          event.target.querySelector('i').className = 'fa fa-clipboard fa-fw';
+          event.target.querySelector('i').className = 'fa fa-clipboard';
         }, 300);
       });
     });
